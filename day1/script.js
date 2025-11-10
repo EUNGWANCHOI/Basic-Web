7e5b621c83eb2ecfaeec37fec9c723822b7b9a76
EUNGWANCHOI <silvergwan@gmail.com>
2025-11-10 17:50:44 +0900

    feat :: make counter feature & update DOM(카운터 기능 만들고 DOM 업데이트 했음)

--- day1/script.js
+++ day1/script.js
@@ -1,3 +1,28 @@
-let btn = document.querySelectorAll("button");
 let result = document.getElementById("result");
+let plusBtn = document.getElementById("plusBtn");
+let minusBtn = document.getElementById("minusBtn");
+let numInput = document.getElementById("numInput");
+let plusInputBtn = document.getElementById("plusInput");
+let minusInputBtn = document.getElementById("minusInput");
+
 let count = 0;
+
+const inputValue = () => {
+  return Number(numInput.value);
+};
+
+const increment = () => {
+  setCount(count + 1);
+};
+
+const decrement = () => {
+  setCount(count - 1);
+};
+
+const setCount = (newValue) => {
+  count = newValue;
+  result.innerText = count;
+};
+
+plusBtn.addEventListener("click", increment);
+minusBtn.addEventListener("click", decrement);
