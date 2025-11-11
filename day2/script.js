2b0b27ec49ac21ca02c261ee07050dce51588e07
EUNGWANCHOI <silvergwan@gmail.com>
2025-11-11 17:51:46 +0900

    feat :: make random number guess(랜덤 숫자 추측 만들기)

--- day2/script.js
+++ day2/script.js
@@ -1,8 +1,31 @@
-let msg = document.getElementById("msg");
-let nameInput = document.getElementById("nameInput");
-let greetBtn = document.getElementById("greetBtn");
+let message = document.getElementById("message");
+let guessInput = document.getElementById("guessInput");
+let guessBtn = document.getElementById("guessBtn");
+let attemptsText = document.getElementById("attempts");
 
 let answer = Math.floor(Math.random() * 100) + 1;
 let attempts = 0;
 
-console.log("정답:", answer);
+function renderMessage(text) {
+  message.innerText = text;
+}
+
+function renderAttempts() {
+  attemptsText.innerText = attempts;
+}
+
+function guess() {
+  let userGuess = Number(guessInput.value);
+  attempts++;
+  renderAttempts();
+
+  if (userGuess === answer) {
+    renderMessage(`정답, ${attempts}번 만에 맞췄음`);
+  } else if (userGuess < answer) {
+    renderMessage("⬆업");
+  } else {
+    renderMessage("⬇다운");
+  }
+}
+
+guessBtn.addEventListener("click", guess);
