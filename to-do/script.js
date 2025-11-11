bd0e9accbb53afc6c963828502366a8f720a3148
EUNGWANCHOI <silvergwan@gmail.com>
2025-11-11 20:33:09 +0900

    feat :: add todo feature(할 일 추가 기능 추가)

--- to-do/script.js
+++ to-do/script.js
@@ -1,3 +1,15 @@
-let addbtn = document.getElementById("add-btn");
+let addBtn = document.getElementById("add-btn");
 let input = document.getElementById("todo-input");
 let list = document.getElementById("todo-list");
+
+const addTodo = () => {
+  const todoText = input.value.trim();
+  if (todoText === "") {
+    alert("할 일을 입력해주세요!");
+  } else {
+    const todo = document.createElement("li");
+    todo.innerText = todoText;
+    list.appendChild(todo);
+    input.value = "";
+  }
+};
