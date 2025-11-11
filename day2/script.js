3468f00f6ce28024096a97bf842bb5f83c4970af
EUNGWANCHOI <silvergwan@gmail.com>
2025-11-11 10:50:00 +0900

    update :: make randome feat & test

---
+++ day2/script.js
@@ -0,0 +1,8 @@
+let msg = document.getElementById("msg");
+let nameInput = document.getElementById("nameInput");
+let greetBtn = document.getElementById("greetBtn");
+
+let answer = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+
+console.log("정답:", answer);
