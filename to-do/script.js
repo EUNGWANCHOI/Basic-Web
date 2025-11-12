6d8100cbc28376a3cc4f27f32c1b5b74e9c6339d
cegwan <silvergwan@gmail.com>
2025-11-12 20:43:48 +0900

    feat :: add checkbox, delete btn(체크 박스 추가 및 삭제 버튼 추가)

--- to-do/script.js
+++ to-do/script.js
@@ -2,15 +2,38 @@ let addBtn = document.getElementById("add-btn");
 let input = document.getElementById("todo-input");
 let list = document.getElementById("todo-list");
 
+/* 할 일 추가 함수 **/
 const addTodo = () => {
   const todoText = input.value.trim();
   if (todoText === "") return alert("할 일을 입력해주세요!");
   else {
     const todo = document.createElement("li");
     todo.textContent = todoText;
+    
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", () => {
+      if (checkbox.checked) {
+        todo.style.textDecoration = "line-through";
+      } else {
+        todo.style.textDecoration = "none";
+      }
+    });
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "❌";
+    deleteBtn.addEventListener("click", () => todo.remove());
+    todo.appendChild(deleteBtn);
+
+    todo.prepend(checkbox);
     list.appendChild(todo);
     input.value = "";
+
+    localStorage.setItem("todos", JSON.stringify(todos));
   }
 };
+
+/* 로컬스토리지 저장 **/
 
 addBtn.addEventListener("click", addTodo);
